50cc5f414a7e7e989cccf06ff3fe1f1f1661bde2
Rho <78846865+loftyyyy@users.noreply.github.com>
2025-11-15 02:08:46 +0800

    Wrapping things up. Done

--- inventory.py
+++ inventory.py
@@ -20,33 +20,42 @@ def main():
                     print(f"Item '{item_name}' already exists in inventory.\n")
                     continue
                 
-                item_price = float(input("Enter item price: "))
-                if(item_price < 0):
-                    print("Price cannot be negative or not a number. Please try again.\n")
-                    continue
+                try:
+                    item_price = float(input("Enter item price: "))
 
-                items.append(item_name)
-                item_prices[item_name] = item_price
+                    if(item_price < 0):
+                        print("Price cannot be negative or not a number. Please try again.\n")
+                        continue
                     
-                print(f"Item '{item_name}' with price {item_price} was added to inventory successfully!\n")
-
+                    items.append(item_name)
+                    item_prices[item_name] = item_price
+                        
+                    print(f"Item '{item_name}' with price {item_price} was added to inventory successfully!\n")
+                except ValueError:
+                    print("Invalid price input. Please enter a valid number.\n")
+                    continue
             elif input_option == "2":
                 item_name = input("Enter item name to update: ")
 
                 if(item_name not in items):
                     print(f"Item '{item_name}' not found in inventory.\n")
                 else:
-                    item_price = float(input("Enter new item price: "))
-
-                    if(item_price < 0): 
-                        print("Price cannot be negative or not a number. Please try again.\n")
-                        continue
-
-                    previous_price = item_prices[item_name]
-                    item_prices[item_name] = item_price
                     
-                    print(f"Price for item '{item_name}', updated from {previous_price} to {item_price}.")
-                    print("Price updated successfully!\n")
+                    try: 
+                        
+                        item_price = float(input("Enter new item price: "))
+
+                        if(item_price < 0): 
+                            print("Price cannot be negative or not a number. Please try again.\n")
+                            continue
+
+                        previous_price = item_prices[item_name]
+                        item_prices[item_name] = item_price
+                        
+                        print(f"Price for item '{item_name}', updated from {previous_price} to {item_price}.")
+                        print("Price updated successfully!\n")
+                    except ValueError:
+                        print("Invalid price input. Please enter a valid number.\n")
 
             elif input_option == "3":
                 print("Exiting the inventory menu. Goodbye!")
